7091ec61968d308c4050ef3ef07fed5a3f627c89
Juicyu <juicyuuu@gmail.com>
2019-04-09 15:16:27 +0300

    Commented code

--- src/filereader.rs
+++ src/filereader.rs
@@ -17,12 +17,15 @@ impl FileReader {
 
     //Read a file and insert its contents to a WordMap
     pub fn read_to_wordmap(&mut self, wl: &mut WordMap) {
+        //Create a string to read the file in
         let mut buffer = String::new();
         self.file.read_to_string(&mut buffer);
 
+        //Split the string from newlines, then split again from whitespaces
         for sentence in buffer.split("\n") {
             let words: Vec<&str> = sentence.split(' ').collect();
 
+            //Add words to the WordMap
             for x in 0..words.len() - 1 {
                 wl.add(words[x].to_string(), words[x + 1].to_string());
             }
